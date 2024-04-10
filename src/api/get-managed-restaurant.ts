0c8cfb75c77514b4b9e9b3db7fb3fbe138094c62
Tiago Behenck dos Santos <tiago.behenck.santos@gmail.com>
2024-04-10 16:15:31 -0300

    get profile and managed restaurant data

---
+++ src/api/get-managed-restaurant.ts
@@ -0,0 +1,17 @@
+import { api } from '@/lib/axios'
+
+interface GetManagedRestaurantResponse {
+  name: string
+  id: string
+  createdAt: Date
+  updatedAt: Date
+  description: string | null
+  managerId: string | null
+}
+export async function getManagedRestaurant() {
+  const response = await api.get<GetManagedRestaurantResponse | undefined>(
+    '/managed-restaurant',
+  )
+
+  return response.data
+}
